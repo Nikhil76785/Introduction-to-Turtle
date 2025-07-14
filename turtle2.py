792839979107b6c4a11cfdf6b65119d55a4e2956
Nikhil76785 <valivetinikhil2@gmail.com>
2025-07-14 19:54:56 +0530

    STAR

---
+++ turtle2.py
@@ -0,0 +1,24 @@
+import turtle
+
+turtle.Screen().bgcolor("aqua")
+turtle.Screen().setup(300, 400)
+
+t = turtle.Turtle()
+
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+
+t.penup()
+t.right(150)
+t.forward(50)
+
+t.pendown()
+t.right(90)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
