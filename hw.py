5aaa6039048a92a9631cbae4409e0c02c1538638
Nikhil76785 <valivetinikhil2@gmail.com>
2025-07-14 20:20:03 +0530

    HW

---
+++ hw.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.Screen().bgcolor("aqua")
+turtle.Screen().setup(300, 400)
+
+t = turtle.Turtle()
+size = 0
+
+t.color("red")
+t.fd(100)
+t.left(90)
+t.color("navyblue")
+t.fd(100)
+t.left(90)
+t.color("green")
+t.fd(100)
+t.left(90)
+t.color("yellow")
+t.fd(100)
